a32ad14a07e420cfa09c2ed4e651530d085340d0
Infochambers <36724526+infochambers@users.noreply.github.com>
2018-03-11 22:22:06 -0700

    Create 1-100-sum.js

---
+++ 1-100-sum.js
@@ -0,0 +1,20 @@
+// The cumulative sum, one through one hundred
+
+// Declare where the total count goes 
+var sum = 0;
+
+// Declare what does the counting
+var x = 0;
+
+//Do the loop to count up to one hundred
+while (x <= 100) {
+  
+  // Add the latest addition to the current count
+  sum = sum + x
+  
+  // increment the count by one
+  x++
+  }
+  
+  //report the final number result
+  console.log(sum)
